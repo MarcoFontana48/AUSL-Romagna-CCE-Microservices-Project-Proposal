24851fcae3aedf38780ad16087b381bb6831fa25
Marco Fontana <marco.fontana98@outlook.it>
2025-07-27 17:27:43 +0200

    refactor(api-gateway): start server on main

--- api_gateway/main.go
+++ api_gateway/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"api_gateway/infrastructure/server"
 	"github.com/MarcoFontana48/AUSL-Romagna-CCE-Microservices-Project-Proposal/utils/log"
 	"log/slog"
 )
 
 func main() {
 	log.InitAsJson()
-
-	slog.Info("api_gateway module started", "module", "api_gateway")
 	slog.Debug("api_gateway module started", "module", "api_gateway")
-	slog.Warn("api_gateway module warning", "module", "api_gateway")
-	slog.Error("api_gateway module error", "module", "api_gateway")
+
+	server.StartServer()
 }
